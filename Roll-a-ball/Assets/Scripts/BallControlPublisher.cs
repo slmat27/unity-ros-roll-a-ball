00e0c41bf3e88e3f70041766855320fc9fc07f3e
slmat27 <ahussein@ing.uc3m.es>
2017-12-17 15:35:13 +0100

    created ROSController, BallControlPublisher & BallPoseSubscriber scripts
    The scripts have the initial and important steps to connect to ROS from Unity and publish and subcribe ... still needs work to be done to make it functional!

---
+++ Roll-a-ball/Assets/Scripts/BallControlPublisher.cs
@@ -0,0 +1,27 @@
+﻿using ROSBridgeLib;
+using ROSBridgeLib.geometry_msgs;
+using SimpleJSON;
+
+public class BallControlPublisher : ROSBridgePublisher
+{
+    // The following three functions are important
+    public new static string GetMessageTopic()
+    {
+        return "/topic/to/publish/to";
+    }
+
+    public new static string GetMessageType()
+    {
+        return "std_msgs/TwistMsg";
+    }
+
+    public static string ToYAMLString(TwistMsg msg)
+    {
+        return msg.ToYAMLString();
+    }
+
+    public static ROSBridgeMsg ParseMessage(JSONNode msg)
+    {
+        return new TwistMsg(msg);
+    }
+}
---
+++ Roll-a-ball/Assets/Scripts/ROSController.cs
@@ -0,0 +1,35 @@
+﻿using ROSBridgeLib;
+using UnityEngine;
+
+public class ROSController : MonoBehaviour
+{
+    private ROSBridgeWebSocketConnection ros = null;
+
+    void Start()
+    {
+        // Where the rosbridge instance is running, could be localhost, or some external IP
+        ros = new ROSBridgeWebSocketConnection("ws://localhost", 9090);
+
+        // Add subscribers and publishers (if any)
+        ros.AddSubscriber(typeof(BallPoseSubscriber));
+        ros.AddPublisher(typeof(BallControlPublisher));
+
+        // Fire up the subscriber(s) and publisher(s)
+        ros.Connect();
+    }
+
+    // Extremely important to disconnect from ROS, otherwise packets continue to flow
+    void OnApplicationQuit()
+    {
+        if (ros != null)
+        {
+            ros.Disconnect();
+        }
+    }
+
+    // Update is called once per frame in Unity
+    void Update()
+    {
+        ros.Render();
+    }
+}
